c5813f377b9e14f827b9ae96c77de0bfaee31be7
najela <achoarnold@live.com>
2014-04-12 15:19:05 +0100

    Clean up Laracasts utilities

--- app/config/app.php
+++ app/config/app.php
@@ -106,7 +106,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Laracasts\Utilities\UtilitiesServiceProvider',
 
 	),
 
