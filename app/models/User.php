a9aa5d8691b4e88f53ba10efbe3eb30f36d53415
najela <achoarnold@live.com>
2014-06-05 20:32:18 +0100

    Migrated to 4.1.29

--- app/models/User.php
+++ app/models/User.php
@@ -19,6 +19,22 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password');
 
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
+
 	/**
 	 * Get the unique identifier for the user.
 	 *
