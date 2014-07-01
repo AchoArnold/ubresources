9040144ee97f59f7b068cc3441922644d8c393a1
najela <achoarnold@live.com>
2014-07-01 13:49:24 +0100

    Front end adjustments for exam timetable

--- app/views/timetables/partials/_header.blade.php
+++ app/views/timetables/partials/_header.blade.php
@@ -1,8 +1,8 @@
 @include('layouts/side-nav')
 
 @section('header')
-   <h1>Time Table</h1>
-    <p>This regularly updated timetable will help to ensure that you don't miss lectures</p>
+   <h1>Exam Time Table</h1>
+    <p>This regularly updated timetable will help to ensure that you don't miss your exams</p>
     @if(Auth::user() && Auth::user()->is_admin())
      		@include('timetables/partials/_admin')
      	@endif
